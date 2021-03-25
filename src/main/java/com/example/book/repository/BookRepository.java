0a0e063cc7e607be1610700574c6bd56072b14eb
renushrestha <renustha18@gmail.com>
2021-03-24 19:31:05 -0500

    Added @Repository annotation

--- src/main/java/com/example/book/repository/BookRepository.java
+++ src/main/java/com/example/book/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.book.repository;
 
 import com.example.book.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 }
