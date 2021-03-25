b3f31b45b9c38b192c683cb8ee0f55c8311f1428
renushrestha <renustha18@gmail.com>
2021-03-24 19:32:41 -0500

    Establish many to one mapping relation with library

--- src/main/java/com/example/book/domain/Book.java
+++ src/main/java/com/example/book/domain/Book.java
@@ -1,18 +1,22 @@
 package com.example.book.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class Book {
     @Id
     @GeneratedValue
-
     private int id;
     private String name;
     private String author;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "libraryId")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Library library;
+
 }
