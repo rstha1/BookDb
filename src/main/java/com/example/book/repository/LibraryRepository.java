049346db4e886c05cab2a25d74d0280e59e58351
renushrestha <renustha18@gmail.com>
2021-03-24 19:25:24 -0500

    Added library Repository interface

---
+++ src/main/java/com/example/book/repository/LibraryRepository.java
@@ -0,0 +1,9 @@
+package com.example.book.repository;
+
+import com.example.book.domain.Library;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibraryRepository extends JpaRepository<Library, Integer> {
+}
